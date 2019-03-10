f64ef4857c6ed724e9b7d5aaf09f7804ea689c9f
Dennys Azevedo <dennys.bd@gmail.com>
2019-03-10 03:35:16 -0300

    Silence check

--- git_inspect/settings/base.py
+++ git_inspect/settings/base.py
@@ -68,9 +68,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 }
 
-# AUTHENTICATION_BACKENDS = (
-#     'django.contrib.auth.backends.ModelBackend',
-# )
+SILENCED_SYSTEM_CHECKS = ('rest_framework.W001',)
 
 ROOT_URLCONF = 'git_inspect.urls'
 
